c79d4177362d9085336d7ec595089408a406d05c
Lorant Pinter <lorant.pinter@prezi.com>
2014-08-24 03:03:52 +0200

    Fixed project name

--- scripts/harfbuzz-js.sh
+++ scripts/harfbuzz-js.sh
@@ -3,4 +3,4 @@
 # export JAVA_HOME=/usr/lib/jvm/java-7-oracle
 unset JAVA_HOME
 
-./gradlew clean harfbuzz-js:uploadArchives --info --stacktrace -Prelease
+./gradlew clean uploadArchives --info --stacktrace -Prelease
